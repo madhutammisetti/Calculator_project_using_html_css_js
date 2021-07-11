1fc7d4f0221c3e5d63e47c302d1846dde4b038e4
madhutammisetti <85747431+madhutammisetti@users.noreply.github.com>
2021-07-11 22:06:30 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+
+let outputScreen = document.getElementById("output_screen");
+
+function display(num){
+outputScreen.value += num;
+}
+function calculate(){
+try{
+    outputScreen.value = eval(outputScreen.value);
+}
+catch(err){
+alert("invalid")
+}
+}
+function clear(){
+outputScreen.value= "";
+}
+function del(){
+outputScreen.value=outputScreen.value.slice(0,-1);
+}
